0430a1577a8d2035ecf65d954cd1c22e6cce1941
alwasychan <alwasy9917@gmail.com>
2025-11-04 23:22:21 +0600

    added new things ;)

---
+++ MaitakeGuruGuru.java
@@ -0,0 +1,17 @@
+
+package main;
+
+
+public class MaitakeGuruGuru {
+
+    
+    public static void main(String args[]) {
+        
+        for(int i = 1; i < 69; i++){
+            
+            System.out.println("Maitake Maitake GuruGuru GuruGuru");  
+            
+        }
+        
+    }
+}
---
+++ Wasychan.java
@@ -0,0 +1,13 @@
+package com.xperia;
+
+import java.util.Scanner;
+public class Wasychan {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter your name: ");
+       String my_name = input.nextLine();    
+       
+        System.out.println("Hello, "+ my_name + " All the best!" );
+    }
+    
+}
